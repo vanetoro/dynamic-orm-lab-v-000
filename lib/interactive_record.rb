2311cde71794d64068aeddcfd76ad21cbef371c6
Vanessa Toro <vanetoro84@gmail.com>
2018-04-30 16:29:50 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def table_name
+      self.to_s.downcase.pluralize
+  end
+
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,28 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
+
+  ATTRIBUTES = {
+    :id => 'INTEGER PRIMARY KEY',
+    :name => 'TEXT',
+    :grade => 'TEXT'
+  }
+
+  def initialize
+
+  end
+
+  def self.table_name
+      self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+     ATTRIBUTES.keys.map do |a|
+      a.to_s
+    end
+  end
 
 end

e7a6aeeb334f170ab9348ac7856daa08714a674a
Vanessa Toro <vanetoro84@gmail.com>
2018-05-01 01:27:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,28 +37,6 @@ ATTRIBUTES = {
   def col_names_for_insert
     self.class.column_names.delete_if { |col| col == "id"  }.join(', ')
 
-  ATTRIBUTES = {
-    :id => 'INTEGER PRIMARY KEY',
-    :name => 'TEXT',
-    :grade => 'TEXT'
-  }
-
-  attr_accessor :id, :name, :grade
-
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
-
-  def self.table_name
-      self.to_s.downcase.pluralize
-  end
-
-  def self.column_names
-     ATTRIBUTES.keys.map do |a|
-      a.to_s
-    end
   end
 
   def table_name_for_insert

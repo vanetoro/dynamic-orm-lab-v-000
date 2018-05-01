c83da44e7153f86ab7c26675afccb0ef978b4dd1
Vanessa Toro <vanetoro84@gmail.com>
2018-05-01 00:29:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -34,9 +34,32 @@ class Student < InteractiveRecord
   end
 
   def col_names_for_insert
-    binding.pry
-    self.class.column_names.delete_if {|col| col = 'id'}.join(" ,")
+    self.class.column_names.delete_if {|col| col == 'id'}.join(", ")
   end
 
+  def values_for_insert
+    values = []
+      self.class.column_names.each do |col_name|
+          values << "'#{send(col_name)}'" unless send(col_name).nil?
+      end
+      values.join(', ')
+  end
+
+  def save
+    sql = "INSERT INTO #{self.table_name_for_insert} (#{self.col_names_for_insert}) VALUES (#{self.values_for_insert})"
+
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0].to_s} = '#{attribute.values[0].to_s}'"
+    DB[:conn].execute(sql)
+  end
 
 end
